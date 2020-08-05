8c02a327ef76f77f03c0d10f8e647c729a7969b7
FabianoRF <freis884@gmail.com>
2020-08-05 16:19:01 -0300

    Componests Input Select and Textarea added

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,7 +4,7 @@ import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css'
 
-const TeacherItem = () => {
+function TeacherItem(){
   return (
       <article className="teacher-item">
         <header>
@@ -15,7 +15,7 @@ const TeacherItem = () => {
             </div>
         </header>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        <br/><br/>
+        <br/>
         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </p>
         <footer>
@@ -28,7 +28,6 @@ const TeacherItem = () => {
                 Entrar em contato
             </button>
         </footer>
-          
       </article>
   )
 }
